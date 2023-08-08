103ceee70b03e7923aff57626ba52b93e43912f7
keenan-chiasson <keenan.chiasson@acstexas.com>
2023-08-08 09:08:56 -0500

    Initial Commit

---
+++ SnowSeeker/SnowSeekerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnowSeekerApp.swift
+//  SnowSeeker
+//
+//  Created by Keenan Chiasson on 8/8/23.
+//
+
+import SwiftUI
+
+@main
+struct SnowSeekerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
