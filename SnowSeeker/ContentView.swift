f80178837d30d4961ff51c7729b5cbec307ef715
keenan-chiasson <keenan.chiasson@acstexas.com>
2023-08-08 10:29:40 -0500

    use alert() and sheet() w/ optionals

--- SnowSeeker/ContentView.swift
+++ SnowSeeker/ContentView.swift
@@ -8,31 +8,45 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var selectedUser: User? = nil
+    @State private var isShowingUser = false
+    
     var body: some View {
-//        NavigationView {
-//            VStack {
-//                Image(systemName: "globe")
-//                    .imageScale(.large)
-//                    .foregroundColor(.accentColor)
-//                Text("Hello, world!")
-//            }
-//            .padding()
-//            .navigationTitle("Primary")
-//
-//            Text("Secondary")
-//        }
-        
-        NavigationView {
-            NavigationLink {
-                Text("New secondary")
-            } label: {
-                Text("Hello, world!")
+        Text("Hello, World!")
+            .onTapGesture {
+                selectedUser = User()
             }
-            .navigationTitle("Primary")
-            
-            Text("Secondary")
-        }
+        // by passing optional, we bind to its nil state
+        // and safely receive unwrapped value w/o nil coalescing
+            .sheet(item: $selectedUser) { user in
+                Text(user.id)
+            }
+        
+        Text("Hello, World!")
+            .onTapGesture {
+                selectedUser = User()
+                isShowingUser.toggle()
+            }
+        // can even pass the optional to an alert
+        // as long as we track boolean state
+            .alert("Welcome", isPresented: $isShowingUser, presenting: selectedUser) { user in
+                Button(user.id) { }
+            }
+        
+        
+        Text("Hello, World!")
+            .onTapGesture {
+                selectedUser = User()
+                isShowingUser.toggle()
+            }
+        // sidebar: can even leave out the OK button,
+        // and SwiftUI will take care of a basic one for us
+            .alert("Welcome", isPresented: $isShowingUser) { }
     }
+}
+
+struct User: Identifiable {
+    var id = "Taylor Swift"
 }
 
 struct ContentView_Previews: PreviewProvider {
